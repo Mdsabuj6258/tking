4285b6ae3e601aca6c0860e8ca91c9d766daf219
mdsabuj6258 <mdsabuj1925@gmail.com>
2024-09-24 18:45:28 +0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(function () {
+
+    "use strict";
+
+    //======NICE SELECT=======
+    $('.select_js').niceSelect();
+
+    //=====datepicker.js======   
+    $(function () {
+        var $startDate = $('.start-date');
+        var $endDate = $('.end-date');
+
+        $startDate.datepicker({
+            autoHide: true,
+        });
+        $endDate.datepicker({
+            autoHide: true,
+            startDate: $startDate.datepicker('getDate'),
+        });
+
+        $startDate.on('change', function () {
+            $endDate.datepicker('setStartDate', $startDate.datepicker('getDate'));
+        });
+    });
+
+    //======= search quntty ========
+    $(".guest_input").on("click", function (event) {
+        $(".qantaty_droofdown").toggleClass("show_qnty");
+        event.stopPropagation();
+    });
+
+    $('body').click(function (event) {
+        if ($(".qantaty_droofdown").hasClass("show_qnty")) {
+            if (!$(event.target).closest('.guest').length) {
+                $(".qantaty_droofdown").removeClass("show_qnty");
+            }
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

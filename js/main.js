53c7d2aab84cb2b762f0b719076e76ab205fda23
mdsabuj6258 <mdsabuj1925@gmail.com>
2024-09-26 01:13:21 +0600

    add 1 othrt section

--- js/main.js
+++ js/main.js
@@ -38,22 +38,49 @@ $(function () {
     });
 
 
-    // // ==========type js==========
-    // var type = new typed(".auto_type", {
-    //     srtings: ["sabuj",
-    //         "nadim",
-    //         "light",
-    //         "fan",
-    //     ]
-    //     typeSpeed: 150,
-    //     backSpeed: 150,
-    //     loop: true
-    // })
 
 
 
+    //======destinations slider====== 
+    $('.destination_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: false,
+        arrows: true,
+        nextArrow: '<i class="far fa-arrow-right nextArrow"></i>',
+        prevArrow: '<i class="far fa-arrow-left prevArrow"></i>',
 
-
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                }
+            }
+        ]
+    });
 
 
 

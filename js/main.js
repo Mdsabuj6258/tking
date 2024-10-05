148f6b68b823f2708ed26fdf0321185aae8afed4
mdsabuj6258 <mdsabuj1925@gmail.com>
2024-10-05 13:12:42 +0600

    add 1 inner page

--- js/main.js
+++ js/main.js
@@ -268,7 +268,46 @@ $(function () {
             }
         ]
     });
+    //======destinations slider====== 
+    $('.room_detels_slider_1').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: false,
+        arrows: true,
+        nextArrow: '<i class="far fa-arrow-right nextArrow"></i>',
+        prevArrow: '<i class="far fa-arrow-left prevArrow"></i>',
 
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                }
+            }
+        ]
+    });
 
     //======= side bar quntty ========
     $(".guest_input").on("click", function (event) {

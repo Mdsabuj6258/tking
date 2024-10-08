fd17b0b1e1be677b19385ac66b09657f6815a9af
mdsabuj6258 <mdsabuj1925@gmail.com>
2024-10-08 17:05:43 +0600

    add 1 inner page

--- js/main.js
+++ js/main.js
@@ -389,7 +389,15 @@ $(function () {
     });
 
 
+    const stars = document.querySelectorAll(".select_rating i");
 
+    stars.forEach((star, index1) => {
+        star.addEventListener("click", () => {
+            stars.forEach((star, index2) => {
+                index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+            });
+        });
+    });
 
 
 

0cc593a113a13edbc3790d2484aafe8e9d0eef91
mdsabuj6258 <mdsabuj1925@gmail.com>
2024-10-25 00:36:23 +0600

    add 2 inner page

--- js/main.js
+++ js/main.js
@@ -514,7 +514,18 @@ $(function () {
     new WOW().init();
 
 
+    //=======COUNTDOWN======   
+    var d = new Date(),
+        countUpDate = new Date();
+    d.setDate(d.getDate() + 365);
 
+    // default example
+    simplyCountdown('.simply-countdown-one', {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate(),
+        enableUtc: true
+    });
 
 
 
